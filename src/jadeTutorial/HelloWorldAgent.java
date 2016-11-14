ded4433882a06b1807ffa71041ab9b4f6da6415f
Mazin Khalid Mohamed Eltayeb <Mazin Khalid Mohamed Eltayeb>
2016-11-14 16:08:26 +0100

    skeleton

---
+++ src/jadeTutorial/HelloWorldAgent.java
@@ -0,0 +1,16 @@
+package jadeTutorial;
+
+import java.util.Iterator;
+
+import jade.core.Agent;
+
+public class HelloWorldAgent extends Agent {
+protected void setup(){
+	System.out.println("Hello World. I’m an agent!"); 
+	System.out.println("My local-name is "+getAID().getLocalName()); 
+	System.out.println("My GUID is "+getAID().getName()); System.out.println("My addresses are:"); 
+	Iterator it = getAID().getAllAddresses(); 
+	while (it.hasNext()) { System.out.println("- "+it.next());
+	}
+}
+}
